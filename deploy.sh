952b41f6a2c1e0f3d7c039b542cba68f7f4d3454
a-j-n <a.gamal@travelyalla.com>
2025-09-21 01:22:54 +0300

    add deploy to ci

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# shellcheck disable=SC2164
+cd /var/www/html/sar7ne ;
+export COMPOSER_ALLOW_SUPERUSER=1;
+rm -rf .env;
+cp .env.server .env;
+git pull origin main --no-ff;
+git reset --hard origin/main;
+composer install ;
+php artisan migrate --seed;
+php artisan config:clear;
+php artisan clear;
+php artisan route:clear;
+sudo chmod -R 777 storage public bootstrap/cache;
+php artisan app:set-telegram-commands ;
