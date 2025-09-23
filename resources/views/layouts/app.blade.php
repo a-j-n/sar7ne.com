d4b33c57f13fd6672b7c9293e02914d42604da4c
a-j-n <a.gamal@travelyalla.com>
2025-09-23 04:18:55 +0300

    auth

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -56,7 +56,7 @@
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
         @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
-            @vite(['resources/css/app.css', 'resources/js/app.js'], ['buildVersion' => time()])
+            @vite(['resources/css/app.css', 'resources/js/app.js'])
         @endif
 
         <link rel="manifest" href="/manifest.json">
