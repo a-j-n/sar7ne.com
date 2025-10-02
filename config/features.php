9be7971a7c220e15a0d04383b0c8f82e814f526b
a-j-n <a.gamal@travelyalla.com>
2025-10-02 17:40:14 +0300

    enhance-deploy-script

--- config/features.php
+++ config/features.php
@@ -2,7 +2,7 @@
 
 return [
     // Toggle the interactive map on the Timeline composer
-    'timeline_map' => env('FEATURE_TIMELINE_MAP', fasle),
+    'timeline_map' => env('FEATURE_TIMELINE_MAP', false),
 
     // Generate image thumbnails for timeline attachments
     'thumbnails' => env('FEATURE_IMAGE_THUMBNAILS', false),
