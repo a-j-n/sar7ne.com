ac7df2eaf132ae2575685fe59b272b4b287ef830
a-j-n <a.gamal@travelyalla.com>
2025-10-11 18:16:29 +0300

    update deploy

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('title', __('Posts'))
+
+@section('content')
+<div class="space-y-6 text-black">
+    <x-ui.card padding="p-8" class="text-center animate-fade-in-up">
+        <h1 class="text-2xl font-semibold">Posts</h1>
+        <p class="mt-2 text-black/70">Coming soon. Your posts will appear here.</p>
+    </x-ui.card>
+</div>
+@endsection
+
