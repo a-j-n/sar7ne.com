c3325986c832bae6f66eaf4a6d14741e33ce015b
a-j-n <a.gamal@travelyalla.com>
2025-10-24 03:51:31 +0300

    AI

--- resources/views/components/ui/card.blade.php
+++ resources/views/components/ui/card.blade.php
@@ -1,5 +1,5 @@
 @props(['class' => '', 'padding' => 'p-6'])
 
 <div {{ $attributes->merge(['class' => 'rounded-3xl border border-slate-200 dark:border-white/10 bg-white text-black text-black-on-white dark:bg-gradient-to-br dark:text-inherit dark:from-white/10 dark:to-slate-900/10 shadow-xl backdrop-blur-sm ' . $padding . ' ' . $class]) }}>
-    {{ $slot }}
+    <p>{{ $slot }}</p>
 </div>
